1d365bcefbc85c9577c6e68f827244285dbf715a
thayer11 <holly.coddington@gmail.com>
2016-07-18 20:34:26 -0600

    seed.rb

---
+++ db/seeds.rb
@@ -0,0 +1,85 @@
+require './models/new_avi.rb'
+
+Avalanches.create({
+	date: "02-07-2016",
+    state: "Colorado",
+    region: "Berthoud Pass",
+    type: "slab",
+    size_crown: "2ft",
+    size_runout: "30ft",
+    temperature: "3 degrees Fahrenheit",
+    temperature_high_past_24hrs: "13 degrees Fahrenheit",
+    temperature_low_past_24hrs: "-2 degrees Fahrenheit",
+    percipitation: ".03"
+    wind_speed: "02mph"
+    wind_direction: "SouthSouthwest"
+    aspect: "NorthWest"
+    elevation: "2,000"
+    snowpack: "faceted"
+    area_description: "The area was to the north of the approach"
+   	route_description: "We veered left at second ridge"
+    trigger: "human triggered"
+    did_you_see_any_avalanches: "yes"
+    did_you_trigger_any_avalanches: "yes"
+    buried: "1"
+    caught: "1"
+    dead: "none"
+    photo_url: "http//:www.photourl"
+    
+})
+
+Avalanches.create({
+	date: "03-11-2016"
+    state: "Colorado"
+    region: "MaroonBells Wilderness"
+    type: "wet slide"
+    size_crown: "no crown"
+    size_runout: "60ft"
+    temperature: "30 degrees Fahreinheit"
+    temperature_high_past_24hrs: "37 degrees Fahreinheit"
+    temperature_low_past_24hrs: "10 degrees Fahreinheit"
+    percipitation: "none"
+    wind_speed: "none"
+    wind_direction: "north"
+    aspect: "NorthWest"
+    elevation: "13,000"
+    snowpack: "Wet"
+    area_description: "steep northwest aspect with moderate amounts of trees."
+   	route_description: "we approached from harpers lot and veered left at the first summit"
+    trigger: "temperature change"
+    did_you_see_any_avalanches: "yes"
+    did_you_trigger_any_avalanches: "yes"
+    buried: "two"
+    caught: "two"
+    dead: "one"
+    photo_url: "http//:www.snowjpg"
+    
+})
+
+Avalanches.create({
+	date: 
+    state:
+    region:
+    type:
+    size_crown:
+    size_runout:
+    temperature:
+    temperature_high_past_24hrs:
+    temperature_low_past_24hrs:
+    percipitation:
+    wind_speed:
+    wind_direction:
+    aspect:
+    elevation:
+    snowpack:
+    area_description:
+   	route_description:
+    trigger:
+    did_you_see_any_avalanches:
+    did_you_trigger_any_avalanches:
+    buried:
+    caught:
+    dead:
+    photo_url:
+    
+})
